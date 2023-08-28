ece14301c9bfdeb1a68d0612a5a6353cd5803625
Allyson Bogo <ab.bogo@gmail.com>
2023-08-28 18:21:40 -0300

    implements exists_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_search = []
+    for file_index in range(0, len(instance)):
+        file = instance.search(file_index)
+        ocorrencias = []
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                ocorrencias.append({"linha": index + 1})
+        if ocorrencias:
+            word_search.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": ocorrencias,
+                }
+            )
+    return word_search
 
 
 def search_by_word(word, instance):
