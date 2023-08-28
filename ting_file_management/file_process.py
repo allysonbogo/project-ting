d76d158fb12cf38cb72ce46f7ebe2bb28ea32868
Allyson Bogo <ab.bogo@gmail.com>
2023-08-28 15:44:59 -0300

    implements remove function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,21 +3,25 @@ import sys
 
 
 def process(path_file, instance):
-    files_queue = instance
     file = txt_importer(path_file)
-    # if not file:
     file_info = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(file),
         "linhas_do_arquivo": file,
     }
-    if file_info not in files_queue:
-        files_queue.enqueue(file_info)
+    if file_info not in instance:
+        instance.enqueue(file_info)
         sys.stdout.write(f"{file_info}\n")
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.is_empty():
+        sys.stdout.write("Não há elementos\n")
+    else:
+        sys.stdout.write(
+            f"Arquivo {instance.dequeue()['nome_do_arquivo']} "
+            "removido com sucesso\n"
+        )
 
 
 def file_metadata(instance, position):
