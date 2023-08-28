3144821bfaf5e2c4846a538d4fae2bf8e3bbdeb1
Allyson Bogo <ab.bogo@gmail.com>
2023-08-28 15:26:49 -0300

    implements process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    files_queue = instance
+    file = txt_importer(path_file)
+    # if not file:
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    if file_info not in files_queue:
+        files_queue.enqueue(file_info)
+        sys.stdout.write(f"{file_info}\n")
 
 
 def remove(instance):
