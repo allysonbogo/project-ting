6c93926a14fe98086a0b564689e77b211b5e7c58
Allyson Bogo <ab.bogo@gmail.com>
2023-08-29 18:31:09 -0300

    refactor word_search to eliminate duplicate functions

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,36 +1,17 @@
 def exists_word(word, instance):
-    word_occurrences = []
-
-    for file_index in range(len(instance)):
-        file = instance.search(file_index)
-        occurrences = []
-
-        for line_index, line in enumerate(file["linhas_do_arquivo"], start=1):
-            if word.lower() in line.lower():
-                occurrences.append({"linha": line_index})
-
-        if occurrences:
-            word_occurrences.append(
-                {
-                    "palavra": word,
-                    "arquivo": file["nome_do_arquivo"],
-                    "ocorrencias": occurrences,
-                }
-            )
-
-    return word_occurrences
+    return search_engine(word, instance, False)
 
 
 def search_by_word(word, instance):
+    return search_engine(word, instance, True)
+
+
+def search_engine(word, instance, content):
     word_occurrences = []
 
     for file_index in range(len(instance)):
         file = instance.search(file_index)
-        occurrences = []
-
-        for line_index, line in enumerate(file["linhas_do_arquivo"], start=1):
-            if word.lower() in line.lower():
-                occurrences.append({"linha": line_index, "conteudo": line})
+        occurrences = generate_info(word, file, content)
 
         if occurrences:
             word_occurrences.append(
@@ -42,3 +23,16 @@ def search_by_word(word, instance):
             )
 
     return word_occurrences
+
+
+def generate_info(word, file, content):
+    occurrences = []
+
+    for line_index, line in enumerate(file["linhas_do_arquivo"], start=1):
+        if word.lower() in line.lower():
+            if content:
+                occurrences.append({"linha": line_index, "conteudo": line})
+            else:
+                occurrences.append({"linha": line_index})
+
+    return occurrences
